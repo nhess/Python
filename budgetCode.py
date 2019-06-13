3218e7be3889cd511b3d7175113c77a6e1fd6ca4
Nate Hess <nathanfhess@gmail.com>
2019-06-12 21:15:01 -0500

    Initial commit 3.0

--- budgetCode.py
+++ budgetCode.py
@@ -7,9 +7,8 @@ with open('04-2019_bank-statement_copy.csv') as csv_budget:
 
 	#TODO: take the 'deposit' keys that have a value and add them all up
 	for line in csv_reader:
-		d = line['deposit']
-		if d != '':
-			currencyRegex = re.sub(r"[/,/\$]", "",d)
+		if line['deposit'] != '':
+			currencyRegex = re.sub(r"[/,/\$]", "",line['deposit'])
 			print(currencyRegex)
 			deposits += float(currencyRegex)
-			print(deposits)
+			print("\t" + str(deposits))
