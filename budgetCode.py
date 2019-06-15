fda5790c120e6d2fa428e674ca6da4f598c4813b
Nate Hess <nathanfhess@gmail.com>
2019-06-14 22:14:01 -0500

    Moved sub Regex into a function, added beginning code for saving added values to xlsx file.

--- budgetCode.py
+++ budgetCode.py
@@ -1,14 +1,28 @@
-import csv, re
+#!/usr/bin/python3
+
+import csv, re, openpyxl
+
+deposits = 0
+
+wb = openpyxl.load_workbook('budget_practice_sheet.xlsx')
+sheet = wb.get_sheet_by_name('Sheet1')
+
+def moneyRegex(money):
+	""" Function to remove money symbols """
+	money = re.sub(r"[/,/\$]", "",money)
+	return money
 
 with open('04-2019_bank-statement_copy.csv') as csv_budget:
 	csv_reader = csv.DictReader(csv_budget)
 
-	deposits = 0
-
-	#TODO: take the 'deposit' keys that have a value and add them all up
+	# Add up all deposits that have numerical values
 	for line in csv_reader:
 		if line['deposit'] != '':
-			currencyRegex = re.sub(r"[/,/\$]", "",line['deposit'])
-			print(currencyRegex)
-			deposits += float(currencyRegex)
-			print("\t" + str(deposits))
+			depositRegex = moneyRegex(line['deposit'])
+			#print(depositRegex)	
+			deposits += float(depositRegex)
+			#print("\t" + str(deposits))
+
+# Place deposit total and place in budget spreadsheet
+sheet.cell(row=3, column=15).value = deposits
+wb.save('budget_practice_sheet.xlsx')
