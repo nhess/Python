427598f33a32b1118e5fa5544f638307bfccfa2c
Nate Hess <nathanfhess@gmail.com>
2019-07-08 20:05:23 -0500

    Started working on returningBudget function, cleaned up some lines with f strings.

--- budgetUserInput.py
+++ budgetUserInput.py
@@ -7,26 +7,26 @@ import sys
 def main():
 
 	username = input("user name: ")
-	filename = username + '_budgetSettings.json'
+	fileName = f'{username}_budgetSettings.json'
 
 	try:
-		with open('userSettings/' + filename) as f_obj:
-			print("Welcome back, " + username + ".  Here are your settings:")
+		with open(f'userSettings/{fileName}') as f_obj:
+			print(f"Welcome back, {username}.  Here are your settings:")
 			settings = json.load(f_obj)
 			# print(settings)
 			#TODO: ask if user wishes to change settings  
 			returningBudget(settings)  
 	except FileNotFoundError:
-		firstBudget(username, filename)
+		firstBudget(username, fileName)
 
 #Set up budget categories
-def firstBudget(user, file):
+def firstBudget(username, fileName):
 	withdrawDict = {}
 	transactions = []
 	regex = re.compile(' ')
 
-	with open('userSettings/' + file, 'w') as f_obj:
-		print("Hello, " + user + "!  Let's make some budget catagories.")
+	with open(f'userSettings/{fileName}', 'w') as f_obj:
+		print(f"Hello, {username}!  Let's make some budget catagories.")
 
 		query = '\nEnter a category for your withdrawals with no spaces between words (Rent, Car_Insurance, etc.).'
 		query += '  Enter q when finished: '
@@ -47,8 +47,23 @@ def firstBudget(user, file):
 
 #TODO: User input for adding up budget categories.
 def returningBudget(settings):
-	pass
-			
+	json_values = []
+
+	with open('04-2019_bank-statement_copy.csv') as csv_budget:
+		csv_reader = csv.DictReader(csv_budget) 
+
+		for key, values in settings.items():
+			for value in values:
+				json_values.append(value)
+
+		# print(json_values)
+
+		for line in csv_reader:
+			# print(line['transaction'])	
+			if line['transaction'] not in json_values:
+				print('Not Found:')
+				print(line['transaction'] + line['withdrawal'])
+	
 def moneyRegex(money):
 	""" Function to remove money symbols """
 	money = re.sub(r"[/,/\$]", "",money)
